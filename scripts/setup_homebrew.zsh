91b5f55670bf865168eacfb321004c99f5719404
Gianni Acciarri <2433206+MadQuint@users.noreply.github.com>
2024-06-29 13:52:43 +0200

    chore: Update Homebrew setup script and configurations
    Refactor the Homebrew setup script so that it is installed in the correct folder for Apple ARM chips.
    
    Previously, using the install script from homebrew would install it in usr/local which is meant for Intel processors. Side effect: install Intel versions of applications when using brew.

--- scripts/setup_homebrew.zsh
+++ scripts/setup_homebrew.zsh
@@ -2,6 +2,25 @@
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Clone Homebrew
+cd /opt
+sudo git clone https://github.com/Homebrew/brew homebrew
 
-brew bundle --verbose
+# Change ownership of the Homebrew directory
+sudo chown -R $(whoami):staff /opt/homebrew
+
+# Update new brew shellenv temporarily
+eval "$(homebrew/bin/brew shellenv)"
+
+# New brew update
+brew update --force --quiet
+
+# Make sure to remove other people's write permissions
+chmod -R go-w "$(brew --prefix)/share/zsh"
+
+# Add new bew shellenv
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Install Homebrew Bundle
+cd ~/.dotfiles
+# brew bundle --verbose
