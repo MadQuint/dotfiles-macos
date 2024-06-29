914d7789c7826e4384ed210775729fd8e9e2bc21
Gianni Acciarri <2433206+MadQuint@users.noreply.github.com>
2024-06-29 13:55:31 +0200

    chore: Update git aliases

--- shell/functions.zsh
+++ shell/functions.zsh
@@ -21,6 +21,9 @@ function droot() {
   cd $(git rev-parse --show-toplevel)
 }
 
+# prompt the AI in the terminal
+# e.g. ask How do I install Homebrew
+# Do not add a question mark and avoid quotes if possible
 function ask() {
 	jq -n \
 		--arg content "$*" \
@@ -40,4 +43,14 @@ function ask() {
 		--header "authorization: Bearer ${PERPLEXITY_API_TOKEN}" \
 		--header 'content-type: application/json' \
 		--data @- | jq --unbuffered --raw-input -j 'gsub("^data: "; "") | gsub("\r$"; "") | select(. != null and . != "") | fromjson | .choices[0].delta.content'
+}
+
+# Similar to `gunwip` but recursive "Unwips" all recent `--wip--` commits not just the last one
+function gunwipall() {
+  local _commit=$(git log --grep='--wip--' --invert-grep --max-count=1 --format=format:%H)
+
+  # Check if a commit without "--wip--" was found and it's not the same as HEAD
+  if [[ "$_commit" != "$(git rev-parse HEAD)" ]]; then
+    git reset $_commit || return 1
+  fi
 }
